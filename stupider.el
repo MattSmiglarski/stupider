fd7945df94c2c4bc4f4a42b837cea2719f9d127a
Mat Smiglarski <penthief@sdf.org>
2014-08-18 23:26:26 +0100

    Remove the buffer from the suggestions list

--- stupider.el
+++ stupider.el
@@ -5,26 +5,64 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Task list
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FIX: Center align
+;; FIX: Pause on question marks and long words etc.
+;; TODO: Read from URL
+;; TODO: Keep history
+;; IDEA: Read from RSS
 ;; TODO: Optimise width
-;; TODO: Run against samples
+;; TODO: Previous word
+;; IDEA: Progress bar
 ;; IDEA: Display zoomed out window on RHS
 ;; DOCS: Animated GIF as a screencast.
 ;; TODO: Speed as WPM
 ;; TODO: Improve punctuation function.
 ;; IDEA: Shortcuts in the echo area
-;; CLEANUP: Frame font in parameters
-;; CLEANUP: Obvious initialisation sequence
 ;; CLEANUP: Configuration params
 ;; CLEANUP: Choose a name
 ;; TODO: Package
-;; TODO: Confirm if already running
 ;; TODO: Determine screen geometry
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'cl)
 
 (defvar stupider-buffer-name "stupider" "The name of the buffer created by `stupider'.")
-(defvar pause-time 1)
+(defvar pause-time 0.2)
+
+(defun center-frame-horizontally (&optional frame)
+  "Center `frame' along the x axis."
+  (interactive)
+  (let* ((f (or frame (selected-frame)))
+         (screen-width (x-display-pixel-width))
+         (frame-width (frame-pixel-width f))
+         (estimated-border-width 16)
+         (required-left (max 0 (/ (- screen-width
+                                     frame-width
+                                     estimated-border-width) 2))))
+    (set-frame-parameter f 'left required-left)
+    (message (format "SCR: %d FR: %d RL: %d"
+                     screen-width
+                     frame-width
+                     required-left))))
+
+(defun center-frame-vertically (&optional frame)
+  "Center `frame' along the y axis."
+  (interactive)
+  (let* ((f (or frame (selected-frame)))
+         (screen-height (x-display-pixel-height))
+         (frame-height (frame-pixel-height f))
+         (estimated-border-height 16)
+         (required-top (max 0 (/ (- screen-height
+                                     frame-height
+                                     estimated-border-height) 2))))
+    (set-frame-parameter f 'top required-top)))
+
+(defun center-frame (&optional frame)
+  "Center `frame'."
+  (interactive)
+  (redisplay)
+  (center-frame-horizontally)
+  (center-frame-vertically))
 
 (defvar stupider-map
   (let ((km (make-sparse-keymap)))
@@ -39,36 +77,46 @@
 (defun stupider-speed-read (source-buffer)
   "Create and run a speed reading frame for `SOURCE-BUFFER'.
 Words from BUFFER are displayed individually and progressed by an adjustable timer."
-  (interactive "bCreate stupider-speed-reader for source buffer: ")
-  (stupider-quit) ; Ensure that we are starting from a clean state.
+  (interactive
+   (list (read-buffer
+          "Create stupider-speed-reader for source buffer: "
+          (remove ;; Don't suggest stupider-buffer
+           (when (boundp 'stupider-buffer)
+             stupider-buffer)
+           (buffer-list))
+          t)))
+  (stupider-quit) ; Ensure that it is starting from a clean state.
   (setq min-pause-time 0.1)
   (setq tokeniser "[^ \n]+")
   (setq *source-buffer* (make-indirect-buffer source-buffer "source buffer"))
-  (setq stupider-frame (make-frame  '((width . 30)
-                                   (height . 1)
-                                   (left . 200)
-                                   (top . 100)
-                                   (mode-line-format . nil)
-                                   (cursor-type . nil)
-                                   (minibuffer . nil)
-                                   (left-margin . 0)
-                                   (left-fringe . 0)
-                                   (right-fringe . 0)
-                                   (tool-bar-lines . 0)
-                                   (menu-bar-lines . 0)
-                                   (line-spacing . 0)
-                                   (fill-column . 30))))
+  (setq stupider-frame
+        (make-frame '((height . 1)
+                      (width . 20)
+                      (top . 100)
+                      (mode-line-format . nil)
+                      (cursor-type . nil)
+                      (minibuffer . nil)
+                      (left-margin . 0)
+                      (left-fringe . 0)
+                      (right-fringe . 0)
+                      (tool-bar-lines . 0)
+                      (menu-bar-lines . 0)
+                      (line-spacing . 0)
+                      (unsplittable . t)
+                      (fill-column . 30))))
   (setq stupider-buffer (get-buffer-create stupider-buffer-name))
   (with-current-buffer *source-buffer*
     (goto-char (point-min))
     (with-selected-frame stupider-frame
-      (display-buffer stupider-buffer '((display-buffer-same-window))))
+      (center-frame stupider-frame)
+      (display-buffer stupider-buffer '((display-buffer-same-window)))
     (stupider--do
      (fundamental-mode)
      (setq buffer-read-only t)
      (use-local-map stupider-map)
-     (set-frame-font (font-spec :size 50))))
-  (stupider--start))
+     (set-frame-font (font-spec :size 50))
+     ))
+  (stupider--start)))
 
 (defmacro stupider--do (&rest body)
   "Do something in the speed reading frame."
